6b5f2e9f6ca210470422a253b848643256120deb
EC2 Default User <ec2-user@ip-172-31-14-109.us-west-2.compute.internal>
2017-12-08 04:50:23 +0000

    Init commit. Test file.

---
+++ cloud9.py
@@ -0,0 +1,14 @@
+'''Learning how to develop with AWS Cloud9
+   This is a test file
+
+   Author: Ron Martin
+'''
+
+import sys
+import os 
+from datetime import datetime
+
+time = datetime.now()
+
+print(str(time))
+
